a67a0fb8c2488d7dab9c4995a50313930bcbbf6a
Ludo Pulles <ludo.pulles@gmail.com>
2018-03-01 21:19:12 +0100

    Calculator score of test cases

--- script.sh
+++ script.sh
@@ -1,12 +1,18 @@
 g++ -std=c++17 -O2 $1
+g++ -std=c++17 -O2 analysis/score.cpp -o calc_score
 echo "A Example";
-./a.out < ./inputs/a_example.in > outputs/a.out;
+./a.out < inputs/a_example.in > outputs/a.out;
+./calc_score outputs/a.out < inputs/a_example.in
 echo "B Should be easy";
-./a.out < ./inputs/b_should_be_easy.in > outputs/b.out;
+./a.out < inputs/b_should_be_easy.in > outputs/b.out;
+./calc_score outputs/b.out < inputs/b_should_be_easy.in
 echo "C No hurry"
-./a.out < ./inputs/c_no_hurry.in > outputs/c.out;
+./a.out < inputs/c_no_hurry.in > outputs/c.out;
+./calc_score outputs/c.out < inputs/c_no_hurry.in
 echo "D Metropolis"
-./a.out < ./inputs/d_metropolis.in > outputs/d.out;
+./a.out < inputs/d_metropolis.in > outputs/d.out;
+./calc_score outputs/d.out < inputs/d_metropolis.in
 echo "E High Bonus"
-./a.out < ./inputs/e_high_bonus.in > outputs/e.out;
+./a.out < inputs/e_high_bonus.in > outputs/e.out;
+./calc_score outputs/e.out < inputs/e_high_bonus.in
 
