c350c8d3f1ec60ba1bcf8bd75ffdaf9167f09722
Ludo Pulles <ludo.pulles@gmail.com>
2018-03-01 19:47:09 +0100

    Modify script

--- script.sh
+++ script.sh
@@ -1,11 +1,12 @@
-
+g++ -std=c++17 -O2 $1
 echo "A Example";
-./Program < ./inputs/a_example.in > ./outputs/a.out;
+./a.out < ./inputs/a_example.in > outputs/a.out;
 echo "B Should be easy";
-./Program < ./inputs/b_should_be_easy.in > ./outputs/b.out;
+./a.out < ./inputs/b_should_be_easy.in > outputs/b.out;
 echo "C No hurry"
-./Program < ./inputs/c_no_hurry.in > ./outputs/c.out
+./a.out < ./inputs/c_no_hurry.in > outputs/c.out;
 echo "D Metropolis"
-./Program < ./inputs/d_metropolis.in > ./outputs/d.out
+./a.out < ./inputs/d_metropolis.in > outputs/d.out;
 echo "E High Bonus"
-./Program < ./inputs/e_high_bonus.in > ./outputs/e.out
+./a.out < ./inputs/e_high_bonus.in > outputs/e.out;
+
