148461598b010d33f2887bf8b60080ffa3fb36e9
Ludo Pulles <ludo.pulles@gmail.com>
2018-03-01 19:35:27 +0100

    Make a greedy solution

--- greedy/main.cpp
+++ greedy/main.cpp
@@ -19,39 +19,72 @@ typedef vector<ii> vii;
 
 ostream& operator<<(ostream &out, const ii &pr) { return out << '(' << pr.x << ',' << pr.y << ')'; }
 
-const int maxRC = 10000, MAXRIDES = 10000;
+const int maxRC = 10000, MAXRIDES = 10000, MAXCARS = 1000;
 
 struct ride_t
 {
 	// time.x: start time, time.y: end time
 	ii fr, to, time;
-	int d; // distance
+	int d, idx; // distance
 };
 
 int R, C, F, N, B, T;
 ride_t rides[MAXRIDES];
+vi carrides[MAXCARS];
 
+inline static int NYdist(ii a, ii b)
+{
+	return abs(a.x - b.x) + abs(a.y - b.y);
+}
 
 void run()
 {
-	cin >> R >> C >> F >> N >> B >> T;
+	scanf("%d %d %d %d %d %d", &R, &C, &F, &N, &B, &T);
+	// cin >> R >> C >> F >> N >> B >> T;
 	REP(i, N) {
-		cin >> rides[i].fr.x >> rides[i].fr.y;
-		cin >> rides[i].to.x >> rides[i].to.y;
-		cin >> rides[i].time.x >> rides[i].time.y;
-		rides[i].d = abs(rides[i].fr.x - rides[i].to.x) + abs(rides[i].fr.y - rides[i].to.y);
+		scanf("%d %d %d %d %d %d", &rides[i].fr.x, &rides[i].fr.y, &rides[i].to.x, &rides[i].to.y, &rides[i].time.x, &rides[i].time.y);
+		// cin >> rides[i].fr.x >> rides[i].fr.y;
+		// cin >> rides[i].to.x >> rides[i].to.y;
+		// cin >> rides[i].time.x >> rides[i].time.y;
+		rides[i].d = NYdist(rides[i].fr, rides[i].to);
 		rides[i].time.y -= rides[i].d;
+		rides[i].idx = i;
 	}
-	
+
+	sort(rides, rides + N, [] (ride_t a, ride_t b) -> bool {
+		return a.time < b.time;
+	});
+
+	vi timeAvailable(F, 0);
+	vii carPos(F, ii());
+	REP(i, N) {
+		int pick = -1;
+		int diff = -1;
+		REP(j, F) {
+			// check if we can match this car with this ride.
+			diff = NYdist(carPos[j], rides[i].fr);
+			int tAtStart = timeAvailable[j] + diff;
+			if (tAtStart <= rides[i].time.y) {
+				pick = j;
+				break;
+			}
+		}
+		if (pick < 0) continue;
+
+		carrides[pick].pb(i);
+		timeAvailable[pick] = min(rides[i].time.x, timeAvailable[i] + diff) + rides[i].d;
+		carPos[pick] = rides[i].to;
+	}
+
+	REP(i, F) {
+		printf("%d", (int) carrides[i].size());
+		for (int j : carrides[i]) printf(" %d", rides[j].idx);
+		printf("\n");
+	}
 }
 
 int main()
 {
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
-
 	run();
-
 	return 0;
 }
