6b1162daabf0d1d4df9f3920ebb36456c3b80840
Max Meijer <meijermax99@gmail.com>
2018-03-01 19:35:10 +0100

    a

---
+++ script.sh
@@ -0,0 +1,11 @@
+
+echo "A Example";
+./Program < ./inputs/a_example.in > ./outputs/a.out;
+echo "B Should be easy";
+./Program < ./inputs/b_should_be_easy.in > ./outputs/b.out;
+echo "C No hurry"
+./Program < ./inputs/c_no_hurry.in > ./outputs/c.out
+echo "D Metropolis"
+./Program < ./inputs/d_metropolis.in > ./outputs/d.out
+echo "E High Bonus"
+./Program < ./inputs/e_high_bonus.in > ./outputs/e.out
